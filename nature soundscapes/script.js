e793df386f7745ad2b68885c3b5a46a8809c33cc
Jandrus000 <jcandrus02@gmail.com>
2023-11-28 12:54:36 -0700

    added background music and mute button

---
+++ nature soundscapes/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    // console.log(searchParams.get('songid'));
+    let audioPath = ""
+    const songid = searchParams.get('songid')
+    if(songid == "ocean"){
+        audioPath = "./audio/ocean waves.mp3"
+    }
+    else if (songid == "forest"){
+        audioPath = "./audio/rain.mp3"
+    }
+
+    const myAudio = new Audio(audioPath);
+    let audioPaused = false;
+
+    myAudio.addEventListener('ended', () => {
+        if (!audioPaused) {
+            myAudio.currentTime = 0;
+            myAudio.play();
+        }
+    });
+
+    myAudio.play();
+
+    const mute_button = document.getElementById("mute-button");
+    mute_button.addEventListener('click', () => {
+        if (audioPaused) {
+            audioPaused = false;
+            myAudio.volume = 1.0; // Unmute audio
+            mute_button.src="./images/mute-icon.png"
+        } else {
+            audioPaused = true;
+            myAudio.volume = 0.0; // Mute audio
+            mute_button.src="./images/umute-icon.png"
+        }
+    });
+});
